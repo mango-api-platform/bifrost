6f1bf9c8f3fab5a3bf36337a0c9a0e0ea0bc37ae
MINT <33680799+kyu1204@users.noreply.github.com>
2023-10-13 20:02:24 +0900

    ✨ Feat: add a proxy modules to the main (#8)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
-fn main() {
-    println!("Hello, world!");
+mod models;
+mod proxy;
+
+use models::RouteInfo;
+use proxy::proxy_request;
+
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
+use warp::Filter;
+
+fn build_routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    let routes_table = Arc::new(RwLock::new(HashMap::new()));
+
+    // Admin endpoint to register a new service
+    let register = {
+        warp::path("register")
+            .and(warp::post())
+            .and(warp::body::json())
+            .and_then({
+                let routes_table_clone = routes_table.clone();
+                move |new_route: RouteInfo| {
+                    let routes_clone = routes_table_clone.clone();
+                    async move {
+                        let mut routes_write = routes_clone.write().unwrap();
+                        let key = new_route.route.clone();
+                        println!("register key: {:?} | value: {:?}", key, new_route);
+                        routes_write.insert(key, new_route);
+                        Ok::<_, warp::Rejection>("Registered")
+                    }
+                }
+            })
+    };
+
+    // Dynamic routing
+    let dynamic_routing = warp::any()
+        .and(warp::header::headers_cloned())
+        .and(warp::path::tail())
+        .and_then({
+            let routes_table_clone = routes_table.clone();
+            move |headers: warp::http::HeaderMap, tail: warp::path::Tail| {
+                let routes_clone = routes_table_clone.clone();
+                let path = tail.as_str().to_string();
+                let routes_read = routes_clone.read().unwrap();
+                let route = path.split('/').next().unwrap().to_string();
+                let remaining_path: String = path.replacen(&route, "", 1);
+                let target = routes_read.get(&route).cloned();
+
+                proxy_request(target, remaining_path, headers)
+            }
+        });
+
+    let routes = register.or(dynamic_routing);
+    routes
+}
+
+#[tokio::main]
+async fn main() {
+    // Start Server
+    println!("Starting the server at 127.0.0.1:3000");
+    warp::serve(build_routes())
+        .run(([127, 0, 0, 1], 3000))
+        .await;
 }
